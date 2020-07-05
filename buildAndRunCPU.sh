6b2f0506814442bfb1a71e13e6a042c1e2441cd3
Foo Ren Xiang <foorenxiang@gmail.com>
2020-07-06 03:54:26 +0800

    changed port mapping for container port 80

--- buildAndRunCPU.sh
+++ buildAndRunCPU.sh
@@ -1,7 +1,7 @@
 docker build --tag foorenxiang/ohr400backendcpu . \
 && docker container prune -f \
 && docker run -ti --name ohr400backendCPUInstance --rm \
- -p 80:80 \
+ -p 81:80 \
  -p 3000:3000 \
  -p 5001:5001 \
  -p 6001:6001 \
