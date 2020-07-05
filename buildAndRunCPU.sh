efb2be7af8831ae609378ccdcc8d19dd29cf2aff
Foo Ren Xiang <foorenxiang@gmail.com>
2020-07-06 01:44:53 +0800

    changed image and container names

--- buildAndRunCPU.sh
+++ buildAndRunCPU.sh
@@ -1,3 +1,3 @@
-docker build --tag foorenxiang/ohr400backend . \
+docker build --tag foorenxiang/ohr400backendcpu . \
 && docker container prune -f \
-&& docker run -ti --name ohr400backendInstance --rm foorenxiang/ohr400backend bash
+&& docker run -ti --name ohr400backendCPUInstance --rm foorenxiang/ohr400backendcpu bash
