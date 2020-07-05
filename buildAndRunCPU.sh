a1871f27aa91623ca64e5dfea16fecc51e48c264
Foo Ren Xiang <foorenxiang@gmail.com>
2020-07-06 03:07:20 +0800

    added container/host port mappings

--- buildAndRunCPU.sh
+++ buildAndRunCPU.sh
@@ -1,3 +1,9 @@
 docker build --tag foorenxiang/ohr400backendcpu . \
 && docker container prune -f \
-&& docker run -ti --name ohr400backendCPUInstance --rm foorenxiang/ohr400backendcpu bash
+&& docker run -ti --name ohr400backendCPUInstance --rm \
+ -p 80:80 \
+ -p 3000:3000 \
+ -p 5001:5001 \
+ -p 6001:6001 \
+ -p 6002:6002 \
+ foorenxiang/ohr400backendcpu bash
