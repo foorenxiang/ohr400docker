62b29433e0a18980bef66c407dcf19828413f3b1
foorenxiang <foorenxiang@gmail.com>
2020-07-07 02:23:22 +0800

    updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@
 # Naturally it can be customised...to perform checks, start various services, etc.,
 # One good use case for the entrypoint would be to pass in a specific script and port to run. in this case a 'master' script like master.q could start on e.g. port 5000 and a set of worker scripts worker-1.q, worker-2.q etc., could run on 5001, 5002...
 echo "Welcome to KDB+ on Docker"
+
+# install any updated python dependencies defined in OHR400Dashboard Repo
+python3 -m pip install -r ~/Sites/OHR400Dashboard/requirements.txt
+
 echo $#
 exec "$@"
