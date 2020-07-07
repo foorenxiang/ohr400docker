3007e4db0dc82fb45c0f2cfd18c02af8bef12490
foorenxiang <foorenxiang@gmail.com>
2020-07-07 09:33:27 +0800

    overwrite dev buildAndRunCPU.sh with production script

--- buildAndRunCPU.sh
+++ buildAndRunCPU.sh
@@ -1,23 +1,9 @@
-# docker build --tag foorenxiang/ohr400backendcpu . \
-# && docker container prune -f \
-# && docker run -ti --name ohr400backendCPUInstance --rm \
-#  -p 81:80 \
-#  -p 3000:3000 \
-#  -p 5001:5001 \
-#  -p 6001:6001 \
-#  -p 6002:6002 \
-#  foorenxiang/ohr400backendcpu bash
-
-
 docker build --tag foorenxiang/ohr400backendcpu . \
 && docker container prune -f \
-&& docker run -ti --name ohr400backendCPUInstance \
+&& docker run -ti --name ohr400backendCPUInstance --rm \
  -p 81:80 \
  -p 3000:3000 \
  -p 5001:5001 \
  -p 6001:6001 \
  -p 6002:6002 \
- --volume ~/docker/OHR400Dashboard:/home/foorx/Sites/OHR400Dashboard \
  foorenxiang/ohr400backendcpu bash
-
- # docker exec -ti ohr400backendCPUInstance bash
